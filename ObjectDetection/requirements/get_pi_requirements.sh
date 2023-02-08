8cb4dd807dd56f3803a4ad2cd3e2a1c8413c1bf5
Seena-02 <seenamohajeran@gmail.com>
2023-02-07 20:53:23 -0800

    update requirements

--- ObjectDetection/requirements/get_pi_requirements.sh
+++ ObjectDetection/requirements/get_pi_requirements.sh
@@ -18,23 +18,30 @@ pip3 install opencv-python==3.4.11.41
 #pip3 install tensorflow
 
 version=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+sys_arch=$(uname -m)
+
 
 if [ $version == "3.9" ]; then
-pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp39-cp39-linux_armv7l.whl
+link="https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp39-cp39-linux_${sys_arch}.whl"
+wget "${link}"
 fi
 
 if [ $version == "3.8" ]; then
-pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp38-cp38-linux_armv7l.whl
+link="https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp38-cp38-linux_${sys_arch}.whl"
+wget "${link}"
 fi
 
 if [ $version == "3.7" ]; then
-pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp37-cp37m-linux_armv7l.whl
+link="https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp37-cp37-linux_${sys_arch}.whl"
+wget "${link}"
 fi
 
 if [ $version == "3.6" ]; then
-pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp36-cp36m-linux_armv7l.whl
+link="https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp36-cp36-linux_${sys_arch}.whl"
+wget "${link}"
 fi
 
 if [ $version == "3.5" ]; then
-pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp35-cp35m-linux_armv7l.whl
+link="https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp35-cp35-linux_${sys_arch}.whl"
+wget "${link}"
 fi
