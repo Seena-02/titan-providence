f854dfa5c5dc361dc702f786f7420626409adcf5
seena <seenamohajeran@gmail.com>
2023-10-05 00:05:34 -0700

    update

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# Drone-Modules
+# QuickStart Guide
 
-## QuickStart Guide
+## To Test Object Detection
+To get started with testing object detection, follow these steps:
 
-Clone the Repo.
+## 1. Clone the Repository
+Begin by cloning the Repo.
 ```
 https://github.com/CSUF-CE-TEAM-1/Drone-Modules.git
 ```
-Condigure Anaconda Env, and install Dependencies.
+## 2. Set Up Environment
+We will primarily use [anaconda](https://www.anaconda.com/download) to manage all packages and dependencies. Create a new conda environment and activate it by running the following commands
 
 ```
 cd Drone-Modules
@@ -18,4 +21,16 @@ conda activate drone-env
 bash get_pi_requirements.sh
 
 ```
+
+## 3. Note for macOS and Windows Users
+Note: If you attempt to replicate this process on macOS or Windows, the installation of the tflite runtime will not succeed. To resolve this, you will need to manually navigate [here](https://github.com/google-coral/pycoral/releases/) and install the tflite_runtime-2.5.0 package.
+
+## 4. Run Webcam Detection
+To run webcam-based object detection, use the following command:
+```
+python3 ObjectDetection/TFLite_detection_webcam.py --modeldir=Sample_TFLite_model
+```
+Feel free to explore and modify the repository to suit your needs. -Titan Providence
+
+
 
--- get_pi_requirements.sh
+++ get_pi_requirements.sh
@@ -18,23 +18,24 @@ pip3 install opencv-python==3.4.11.41
 #pip3 install tensorflow
 
 version=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+os_type=$(uname -m)
 
 if [ $version == "3.9" ]; then
-pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp39-cp39-linux_armv7l.whl
+pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp39-cp39-linux_${os_type}.whl
 fi
 
 if [ $version == "3.8" ]; then
-pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp38-cp38-linux_armv7l.whl
+pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp38-cp38-linux_${os_type}.whl
 fi
 
 if [ $version == "3.7" ]; then
-pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp37-cp37m-linux_armv7l.whl
+pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp37-cp37m-linux_${os_type}.whl
 fi
 
 if [ $version == "3.6" ]; then
-pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp36-cp36m-linux_armv7l.whl
+pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp36-cp36m-linux_${os_type}.whl
 fi
 
 if [ $version == "3.5" ]; then
-pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp35-cp35m-linux_armv7l.whl
+pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp35-cp35m-linux_${os_type}.whl
 fi
